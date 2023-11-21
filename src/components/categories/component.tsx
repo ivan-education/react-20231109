ebc85b80d4c9e68dc0c4d0fac748fa851b527ff6
Ivan Bursky <iburskiy@gmail.com>
2023-11-22 01:30:37 +0300

    add active button logic to Categories

--- src/components/categories/component.tsx
+++ src/components/categories/component.tsx
@@ -2,6 +2,7 @@ import { Category } from "src/components/category/component";
 import classes from "./styles.module.css";
 import restaurantPageStyles from "src/pages/restaurants-page/styles.module.css";
 import { CategoryEntity } from "src/types";
+import { useState } from "react";
 
 interface Props {
   categories: CategoryEntity[];
@@ -12,6 +13,7 @@ export const Categories: React.FC<Props> = ({
   categories,
   onCategorySelect,
 }) => {
+  const [activeCategoryId, setActiveCategoryId] = useState("");
   return (
     <div className={`${classes.categories} ${restaurantPageStyles.indent}`}>
       {categories.map((category) => {
@@ -19,7 +21,11 @@ export const Categories: React.FC<Props> = ({
           <Category
             key={category.id}
             category={category}
-            onClick={() => onCategorySelect(category)}
+            onClick={() => {
+              onCategorySelect(category);
+              setActiveCategoryId(category.id);
+            }}
+            isActive={category.id === activeCategoryId}
           />
         );
       })}
--- src/components/category/component.tsx
+++ src/components/category/component.tsx
@@ -1,10 +1,21 @@
 import { CategoryEntity } from "src/types";
+import classes from "./styles.module.css";
 
 interface Props {
   category: CategoryEntity;
+  isActive: boolean;
   onClick: () => void;
 }
 
-export const Category: React.FC<Props> = ({ category, onClick }) => {
-  return <div onClick={onClick}>{category.name}</div>;
+export const Category: React.FC<Props> = ({ category, isActive, onClick }) => {
+  return (
+    <button
+      className={`${classes.category} ${
+        isActive ? classes.category_active : ""
+      }`}
+      onClick={onClick}
+    >
+      {category.name}
+    </button>
+  );
 };
