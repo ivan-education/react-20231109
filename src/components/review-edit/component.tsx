e2dfb88346785e8eee401228080d94ae263933f9
ivan-education <150901794+ivan-education@users.noreply.github.com>
2023-12-23 13:56:07 +0300

    add a reducer and autofocus to ReviewEdit (remarks)

--- src/components/review-edit/component.tsx
+++ src/components/review-edit/component.tsx
@@ -2,35 +2,63 @@ import { RATING_MIN, RATING_STEP } from "src/constants/constants";
 import classes from "./styles.module.scss";
 import { Button } from "../button/component";
 import { Counter } from "../counter/component";
-import { useRef, useState } from "react";
+import { useReducer } from "react";
 
 interface Props {
   text: string;
   rating: number;
   onSave: (a: string, b: number) => void;
 }
-export const ReviewEdit: React.FC<Props> = ({ text, rating, onSave }) => {
-  const [newText, setNewText] = useState(text);
-  const [newRating, setNewRating] = useState(rating);
 
-  const ref = useRef<HTMLInputElement>(null);
+enum ACTIONS {
+  UPDATE_TEXT = "updateText",
+  UPDATE_RATING = "updateRating",
+}
 
-  setTimeout(() => {
-    if (ref.current != null) {
-      ref.current.focus();
+type FormState = {
+  newText: string;
+  newRating: number;
+};
+
+type FormAction =
+  | { type: ACTIONS.UPDATE_TEXT; payload: string }
+  | { type: ACTIONS.UPDATE_RATING; payload: number };
+
+function reducer(state: FormState, action: FormAction) {
+  switch (action.type) {
+    case ACTIONS.UPDATE_TEXT: {
+      return {
+        newText: action.payload,
+        newRating: state.newRating,
+      };
     }
+    case ACTIONS.UPDATE_RATING: {
+      return {
+        newText: state.newText,
+        newRating: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const ReviewEdit: React.FC<Props> = ({ text, rating, onSave }) => {
+  const [reviewEditForm, dispatch] = useReducer(reducer, {
+    newText: text,
+    newRating: rating,
   });
 
   return (
     <div>
       <div>
-        <input
+        <textarea
+          autoFocus
           className={classes.reviewEdit__input}
-          ref={ref}
-          type="text"
-          value={newText}
+          value={reviewEditForm.newText}
           onChange={(e) => {
-            setNewText(e.target.value);
+            dispatch({ type: ACTIONS.UPDATE_TEXT, payload: e.target.value });
           }}
         />
       </div>
@@ -38,8 +66,10 @@ export const ReviewEdit: React.FC<Props> = ({ text, rating, onSave }) => {
         <div className={classes.reviewEdit__form}>
           <label className={classes.reviewEdit__ratingLabel}>Rating:</label>
           <Counter
-            count={newRating}
-            onUpdateCount={(count) => setNewRating(count)}
+            count={reviewEditForm.newRating}
+            onUpdateCount={(count) => {
+              dispatch({ type: ACTIONS.UPDATE_RATING, payload: count });
+            }}
             step={RATING_STEP}
             min={RATING_MIN}
           />
@@ -47,7 +77,7 @@ export const ReviewEdit: React.FC<Props> = ({ text, rating, onSave }) => {
         <Button
           className={classes.reviewEdit__saveBtn}
           onClick={() => {
-            onSave(newText, newRating);
+            onSave(reviewEditForm.newText, reviewEditForm.newRating);
           }}
         >
           Save
